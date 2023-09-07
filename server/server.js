771b76cc053e7a08894172b21ce05dd8c4280420
Akshay <98738875+finiky@users.noreply.github.com>
2023-09-08 02:14:21 +0530

    added an testing get route for restaurants. installed nodemon

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+
+app.get("/restaurants", async(request, response) => {
+  try {
+    console.log("Retrive the restaurants");
+  } catch(error) {
+    next(error);
+  }
+});
+
+
+
+
+//start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server is running on port 5000");
