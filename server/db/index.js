70f3fd7daca9b8585e8d43c0283cb8580ebd6001
Akshay <98738875+finiky@users.noreply.github.com>
2023-09-08 04:24:24 +0530

    removed the conetent from the pool :

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,5 @@
 const { Pool } = require("pg");
-const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-});
+const pool = new Pool();
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
