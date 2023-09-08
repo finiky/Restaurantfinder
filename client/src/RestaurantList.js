0841f934a5024513ecfabb5a3135865f425dbae2
Akshay <98738875+finiky@users.noreply.github.com>
2023-09-09 01:25:49 +0530

    created DELETE and UPDATE react component

--- client/src/RestaurantList.js
+++ client/src/RestaurantList.js
@@ -1,7 +1,9 @@
 import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { RestaurantContext } from "./contextApi/RestaurantsContext";
 const RestaurantList = (props) => {
   const { restaurants, setRestaurants } = useContext(RestaurantContext);
+  const navigate = useNavigate();
   useEffect(() => {
     try {
       const fetchData = async () => {
@@ -12,6 +14,31 @@ const RestaurantList = (props) => {
       fetchData();
     } catch (error) {}
   }, []);
+  const handleDelete = async (restaurantId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/restaurants/${restaurantId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "CONTENT-TYPE": "application/json",
+          },
+        }
+      );
+      setRestaurants(
+        restaurants.filter((restaurant) => restaurant.id !== restaurantId)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleUpdate = async (restaurantId) => {
+    try {
+      navigate(`/restaurants/${restaurantId}/update`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="list-group">
       <table className="table table-dark table-hover">
@@ -26,20 +53,33 @@ const RestaurantList = (props) => {
           </tr>
         </thead>
         <tbody>
-          {restaurants && restaurants.map((restaurant) => (
-            <tr key={restaurant.id}>
-              <td>{restaurant.name}</td>
-              <td>{restaurant.location}</td>
-              <td>{"$".repeat(restaurant.price_range)}</td>
-              <td>reviews</td>
-              <td>
-                <button className="btn btn-warning">Edit</button>
-              </td>
-              <td>
-                <button className="btn btn-danger">Delete</button>
-              </td>
-            </tr>
-          ))}
+          {restaurants &&
+            restaurants.map((restaurant) => (
+              <tr key={restaurant.id}>
+                <td>{restaurant.name}</td>
+                <td>{restaurant.location}</td>
+                <td>{"$".repeat(restaurant.price_range)}</td>
+                <td>reviews</td>
+                <td>
+                  <button
+                    className="btn btn-warning"
+                    onClick={() => {
+                      handleUpdate(restaurant.id);
+                    }}
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(restaurant.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
--- client/src/routes/UpdateRestaurant.js
+++ client/src/routes/UpdateRestaurant.js
@@ -1,5 +1,74 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 const UpdateRestaurant = () => {
-  return <div></div>;
+  const { id } = useParams();
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [price_range, setPriceRange] = useState("");
+  useEffect(() => {
+    const getRestaurant = async () => {
+      const response = await fetch(
+        `http://localhost:5000/api/restaurants/${id}`
+      );
+      const data = await response.json();
+      setName(data.data.name);
+      setLocation(data.data.location);
+      setPriceRange(data.data.price_range);
+    };
+    getRestaurant();
+  }, [id]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      `http://localhost:5000/api/restaurants/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "CONTENT-TYPE": "application/json",
+        },
+        body: JSON.stringify({ name, location, price_range }),
+      }
+    );
+  };
+  return (
+    <div>
+      <h1 className="text-center">Update Restaurant</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="location">Location</label>
+            <input
+              id="location"
+              className="form-control"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="priceRange">Price Range</label>
+            <input
+              id="price_range"
+              className="form-control"
+              value={price_range}
+              onChange={(e) => setPriceRange(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Update
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default UpdateRestaurant;
